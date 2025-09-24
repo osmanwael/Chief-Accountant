5df540c3b99d373367f0b0bf56ce157aadb96ac5
osmanwael <osmanwael0887@gmail.com>
2025-09-24 13:42:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".typing", {
+        strings: ["مدير مالي", "مدير حسابات", "رئيس حسابات"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+});
